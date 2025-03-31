60d7894b1e541d3f4cb430546d1d88c4d8af19cc
Mitchell Tol <mitchell.tol@moveagency.com>
2025-03-31 10:02:41 +0200

    Chore: add coordinates

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sys
+import time
+import os
 from utils import parse_parameters
 
 def main(args: dict):
-    print(args)
+    os.system('adb shell am stop-app "com.moveagency.myterminal"')
+    os.system('adb shell am start -n "com.moveagency.myterminal/com.moveagency.myterminal.MainActivity"')
+    time.sleep(5)
+    for _ in range(6):
+        os.system('adb shell input swipe 550 2146 550 338')
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     args = parse_parameters(sys.argv)
